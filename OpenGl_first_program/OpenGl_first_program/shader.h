af09ae4a175550201aa867a8857051b9bf387179
Denis <denis.furyaev@gmail.com>
2020-09-04 13:08:58 +0300

    new shader class

---
+++ OpenGl_first_program/OpenGl_first_program/shader.h
@@ -0,0 +1,13 @@
+//
+//  shader.h
+//  OpenGl_first_program
+//
+//  Created by Denis Furyaev on 03.09.2020.
+//  Copyright © 2020 Denis Furyaev. All rights reserved.
+//
+
+#ifndef shader_h
+#define shader_h
+
+
+#endif /* shader_h */
